a739f29188c9b1c78cb700e4f6db7d15df156a33
Sunkai <ksun37754@gmail.com>
2026-04-05 11:14:50 +0800

    Remove basePath and update build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/customer-label',
   experimental: {
     serverActions: {
       bodySizeLimit: '2mb',
